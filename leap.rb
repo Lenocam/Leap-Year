4d2852d09d892abbc8e6db017d542657c4a6fa9e
Reed S. MacConnell <learn22ruby@gmail.com>
2013-08-01 11:48:26 -0400

    Still using the answer key for help, but I'm understanding it better.

--- leap.rb
+++ leap.rb
@@ -1,13 +1,21 @@
-puts 'enter year'
-year = gets.chomp.to_i
 
-if year % 4 == 0
-	if year % 100 != 0 || year % 400 == 0
-	puts 'it\'s a leap year'
+puts 'Find out which years are leap years over any span of time.'
+puts 'Get a starting year:'
+starting = gets.chomp.to_i
+puts 'Excellent! Pick and ending year.'
+ending = gets.chomp.to_i
+puts 'Check it out... these years are leap years:'
 
-	end
-else
-	puts 'not a leap year'	
-end 
+year = starting
+
+while year <= ending
+	if year % 4 == 0
+		if year % 100 != 0 || year % 400 == 0
+			puts year
+		end	
+	end 
+
+	year = year + 1
+end
 
 
