6b31fb294c79630fc88cfb5ba57a14b6ac7a88db
Reed S. MacConnell <learn22ruby@gmail.com>
2013-08-01 11:18:59 -0400

    got code to do math right

--- leap.rb
+++ leap.rb
@@ -1,16 +1,13 @@
 puts 'enter year'
 year = gets.chomp.to_i
 
-if year % 4 == 0 && if year % 100 != 0
-	if year % 100 == 0
-		year = fCheck
-		if fCheck % 400 == 0
-			puts 'it\'s a leap year'
-			
-		end
-		
-	end	
-end	
+if year % 4 == 0
+	if year % 100 != 0 || year % 400 == 0
+	puts 'it\'s a leap year'
+
+	end
 else
-	puts 'Leap Year this is not.'
-end
+	puts 'not a leap year'	
+end 
+
+
